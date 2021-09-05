3ef5450fb5328c474a33ddba8e2abf946e6e7dd4
Pcarioca <63117204+Pcarioca@users.noreply.github.com>
2021-09-06 01:30:17 +0300

    idk

--- jquery/index.js
+++ jquery/index.js
@@ -1,6 +1,24 @@
-$(function(){
-    $('#li').hover(
-        function() {    $("audio")[0].play();},
-        function() {    $("audio")[0].pause();}
-        )
+
+
+
+
+$(document).ready(function () {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('id', 'play');
+    audioElement.setAttribute('src', 'audio/a3.mp3');
+    audioElement.setAttribute('autoplay', 'autoplay');
+    $.get();
+    $("#body").append(audioElement);
+    
+$('#/*').on('hover', function() {
+        audioElement.play();
     });
+    var audio = $("#play")[0];
+    $("nav a").mouseenter(function() {
+      audio.play();
+    });
+
+
+    
+
+});
